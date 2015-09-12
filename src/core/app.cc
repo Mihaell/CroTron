45012b423d8e65a0217b5ccafcfd32ccda8e2724
Tonko Sabolcec <tonkosi123@gmail.com>
2015-09-12 18:21:29 +0200

    Add CroTron image to test Texture asset.

--- src/core/app.cc
+++ src/core/app.cc
@@ -1,12 +1,19 @@
 #include "core/app.h"
 
+#include <SFML/Graphics.hpp>
+
+#include "assets/texture.h"
+
 App::App() {
-
   window.create(sf::VideoMode(960, 510), "CroTron");
-
 }
 
 void App::Run() {
+  Texture::Load("CROTRON_LOGO", "data/textures/crotron.png");
+
+  sf::Sprite sprite;
+  sprite.setTexture(*Texture::Get("CROTRON_LOGO"));
+  sprite.setPosition(100, 100);
 
   while (window.isOpen()) {
     sf::Event event;
@@ -15,7 +22,7 @@ void App::Run() {
         window.close();
 
     window.clear();
+    window.draw(sprite);
     window.display();
   }
-
 }
